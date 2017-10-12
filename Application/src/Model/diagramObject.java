0e26f57705baa7424f1c594194d9ad390ee45f04
Rashadse <rashadse@hotmail.com>
2017-10-12 22:34:44 +0200

    parsing processes in the provided json file now works

--- Application/src/Model/diagramObject.java
+++ Application/src/Model/diagramObject.java
@@ -1,51 +1,179 @@
 package Model;
 
+//import javax.swing.text.AbstractDocument;
 import java.util.List;
 
 public class diagramObject {
 
-    public meta meta;
-    String type;
-    List<Processes> processes;
-    //   diagram diagram;
 
-    public String getType() {
-        return type;
-    }
+    //handle diagram's meta
+    public meta meta;
+
+    //handle the Type of the diagram
+    String type;
 
     public void setType(String type) {
         this.type = type;
     }
 
+    public String getType() {
+        return type;
+    }
+
+    //handle the Processes of the diagram
+    List<Processes> processes;
+
+    public void setProcesses(List<Processes> processes) {
+        this.processes = processes;
+    }
+
+    public List<Processes> getProcesses() {
+        return processes;
+    }
+
+
+    //handle the messages of the diagram
+//    List<diagram> diagram;
+//    public List<diagramObject.diagram> getDiagram() {
+//        return diagram;
+//    }
+//
+//    public void setDiagram(List<diagramObject.diagram> diagram) {
+//        this.diagram = diagram;
+//    }
+//
 
 
     public class meta{
 
         public String format;
         String version;
-        //?? extensions;
+        //TODO : find out what the purpose is
+        List<String> extensions;
+
+        public void setExtensions(List<String> extensions) {
+            this.extensions = extensions;
+        }
+
+        public List<String> getExtensions() {
+            return extensions;
+        }
+
 
         public String getFormat() {
             return format;
         }
-
         public void setFormat(String format) {
             this.format = format;
         }
-
         public String getVersion() {
             return version;
         }
-
         public void setVersion(String version) {
             this.version = version;
         }
 
 
+    }
+
+    public class Processes {
+
+        String name;
+        String class1;
+
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getClass1() {
+            return class1;
+        }
+
+        public void setClass1(String class1) {
+            this.class1 = class1;
+        }
+
+
+
 
     }
-    //private class diagram{
-        //String node;
-        //       List<AbstractDocument.Content> content;
- //   }
+
+//    public class diagram {
+//
+//        String node;
+//        List<Content> content;
+//
+//        public String getNode() {
+//            return node;
+//        }
+//
+//        public void setNode(String node) {
+//            this.node = node;
+//        }
+//
+//
+//
+//        public List<diagramObject.Content> getContent() {
+//            return content;
+//        }
+//
+//        public void setContent(List<diagramObject.Content> content) {
+//            this.content = content;
+//        }
+//
+//
+//    }
+
+//    public class Content {
+//
+//        String node;
+//        String from;
+//        String to;
+//        List<String> message;
+//
+//        public String getNode2() {
+//            return node;
+//        }
+//
+//        public void setNode2(String node) {
+//            this.node = node;
+//        }
+//
+//
+//        public String getFrom() {
+//            return from;
+//        }
+//
+//        public void setFrom(String from) {
+//            this.from = from;
+//        }
+//
+//
+//        public String getTo() {
+//            return to;
+//        }
+//
+//        public void setTo(String to) {
+//            this.to = to;
+//        }
+//
+//
+//        public List<String> getMessage() {
+//            return message;
+//        }
+//
+//        public void setMessage(List<String> message) {
+//            this.message = message;
+//        }
+//
+//
+//
+//    }
+
+
 }
--- Application/src/Parser.java
+++ Application/src/Parser.java
@@ -1,16 +1,30 @@
 import Model.diagramObject;
 import com.google.gson.Gson;
 
+
+
+
 public class Parser {
- public static void Parser(String inputJSON){
-     Gson gson = new Gson();
+    public static void Parser(String inputJSON){
+        Gson gson = new Gson();
+        diagramObject parsedDiagram;
 
-    diagramObject parsedDiagram = gson.fromJson(inputJSON, Model.diagramObject.class);
+        parsedDiagram = gson.fromJson(inputJSON, Model.diagramObject.class);
 
-    System.out.println("Isabelle is old and blind, needs help to see where output starts");
-    System.out.println("Format " + parsedDiagram.meta.getFormat());
-    System.out.println("Version " + parsedDiagram.meta.getVersion());
-    System.out.println("Type " + parsedDiagram.getType());
+        System.out.println("Isabelle is old and blind, needs help to see where output starts");
+        System.out.println("Format " + parsedDiagram.meta.getFormat());
+        System.out.println("Version " + parsedDiagram.meta.getVersion());
+        System.out.println("Type " + parsedDiagram.getType());
 
- }
+        for (diagramObject.Processes element : parsedDiagram.getProcesses() ) {
+            System.out.println(element.getClass1());
+            System.out.println(element.getName());
+        }
+
+//        for (diagramObject.diagram element : parsedDiagram.getDiagram() ) {
+//            System.out.println(element.getNode());
+//            System.out.println(element.getContent());
+//        }
+
+    }
 }
