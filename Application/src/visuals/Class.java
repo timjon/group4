7c5620a2ca41a392c8925c9c2c9c88604b61b051
Pontus Laestadius <pontus.laestadius@gmail.com>
2017-10-12 16:15:48 +0200

    added better lifelines

--- Application/src/visuals/Class.java
+++ Application/src/visuals/Class.java
@@ -45,14 +45,20 @@ public class Class implements Renderable {
         int y = this.coordinates.getY();
 
         // Lifeline
-        gc.setFill(Color.BEIGE);
+        Image platform = new Image("resources/platform.png");
+        Image pillar = new Image("resources/pillar.png");
+
         int lifeline_width = size/12;
-        gc.strokeRect(x +size/4 -lifeline_width,y + size/6,lifeline_width,1000);
-        gc.setFill(Color.BLACK);
+        int scale = (int) (pillar.getHeight()/lifeline_width);
+        for (int i = 0; i < 1000; i+=scale) {
+            gc.drawImage(pillar,x +size/4 -lifeline_width,y + size/6 +i,lifeline_width,scale);
+        }
+
+        gc.drawImage(platform,x -size/8,y + size/2, size/2 + size/4,size/6);
+
 
         // Object
         gc.drawImage(image, x, y, size/2, size/2);
-
 
         int len = this.name.length();
 
