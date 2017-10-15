4f815ef81aa1010b661fa3d1d3ed635880a6c118
Rashadse <rashadse@hotmail.com>
2017-10-15 14:36:39 +0200

    fixed issue with assigning the word string to a class, added prasing handlers for Diagram, Content and Messages

--- Application/src/Parser.java
+++ Application/src/Parser.java
@@ -11,20 +11,28 @@ public class Parser {
 
         parsedDiagram = gson.fromJson(inputJSON, Model.diagramObject.class);
 
-        System.out.println("Isabelle is old and blind, needs help to see where output starts");
-        System.out.println("Format " + parsedDiagram.meta.getFormat());
-        System.out.println("Version " + parsedDiagram.meta.getVersion());
-        System.out.println("Type " + parsedDiagram.getType());
+        System.out.println("\n" + "Meta: \n");
+        diagramObject.Meta metaElement = parsedDiagram.getMeta();
+        System.out.println(metaElement.getFormat() + "\n" + metaElement.getVersion() + "\n" + parsedDiagram.getType());
 
-        for (diagramObject.Processes element : parsedDiagram.getProcesses() ) {
-            System.out.println(element.getClass1());
-            System.out.println(element.getName());
+
+        System.out.println("\n" + "Processes: \n");
+        for (diagramObject.Processes processesElement : parsedDiagram.getProcesses()) {
+            System.out.println( processesElement.getClass1() + " : " + processesElement.getName()+ " \n ");
         }
 
-//        for (diagramObject.diagram element : parsedDiagram.getDiagram() ) {
-//            System.out.println(element.getNode());
-//            System.out.println(element.getContent());
-//        }
 
-    }
+        System.out.println("\n" + "Diagram Elements: \n");
+        for (diagramObject.ContentArray diagramElement : parsedDiagram.getDiagram().getContent().get(0).getContent()) {
+            System.out.println(diagramElement.getFrom() + " to " + diagramElement.getTo() + " " + diagramElement.getNode() + "s: [" + diagramElement.getMessage().get(0) + ", " + diagramElement.getMessage().get(1) + ", " + diagramElement.getMessage().get(2) + "] \n ");
+        }
+
+
+
+        System.out.println("\n" + "Parallel Diagram Elements: \n");
+        for (diagramObject.ContentArray parallelDiagramElement : parsedDiagram.getDiagram().getContent().get(1).getContent()) {
+            System.out.println(parallelDiagramElement.getFrom() + " to " + parallelDiagramElement.getTo() + " " + parallelDiagramElement.getNode() + "s: [" + parallelDiagramElement.getMessage().get(0) + "] \n ");
+        }
+		
+	}
 }
