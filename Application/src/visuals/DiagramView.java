0fec151ea2ec38ec46c465b5961602b32e15d254
Pontus Laestadius <pontus.laestadius@gmail.com>
2017-10-16 13:25:28 +0200

    updates draw

--- Application/src/visuals/DiagramView.java
+++ Application/src/visuals/DiagramView.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 /*
  * Handles a single tabbed diagram view and it's state
- * @version 0.2
+ * @version 0.3
  * @author Pontus Laestadius
  */
 public class DiagramView {
@@ -17,7 +17,6 @@ public class DiagramView {
     private Tab tab;
     public static ArrayList<DiagramView> list = new ArrayList<>(); // A list of all Diagram views.
     public static TabPane tabPane;
-
 
     public DiagramView(Draw draw, String tabName) {
         this.draw = draw;
@@ -35,10 +34,15 @@ public class DiagramView {
     void resize(int w, int h) {
         // By adding the Resizing state and checking for it, All concurrency related bugs were squashed.
         if (this.state == State.RESIZING) return;
-        State tmp = this.state;
-        this.state = State.RESIZING;
+        State tmp = setState(State.RESIZING);
         draw.resize(w,h);
         this.state = tmp;
+    }
+
+    State setState(State state) {
+        State tmp = this.state;
+        this.state = state;
+        return tmp;
     }
 
 }
