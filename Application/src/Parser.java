4ff6873032d9503fc5c33ae8801c94d8c74e48b8
Rashadse <rashadse@hotmail.com>
2017-10-18 13:15:57 +0200

    Return proper String format by the parser to be used by the backend

--- Application/src/Parser.java
+++ Application/src/Parser.java
@@ -5,10 +5,26 @@ import com.google.gson.Gson;
 
 
 public class Parser {
+
+    // odd number for the First Diagram counter
+    static int counter = 1;
+    // even number for the parallel Diagram counter
+    static int parallelCounter = 2;
+
+    static String Processes = "";
+    static String properProcesses = "";
+
+    static String FirstDiagram = "";
+    static String properFirstDiagram = "";
+
+    static String ParallelDiagram= "";
+    static String ProperParallelDiagram = "";
+
+
+
     public static void Parser(String inputJSON){
         Gson gson = new Gson();
         diagramObject parsedDiagram;
-
         parsedDiagram = gson.fromJson(inputJSON, Model.diagramObject.class);
 
         System.out.println("\n" + "Meta: \n");
@@ -18,13 +34,16 @@ public class Parser {
 
         System.out.println("\n" + "Processes: \n");
         for (diagramObject.Processes processesElement : parsedDiagram.getProcesses()) {
-            System.out.println( processesElement.getClass1() + " : " + processesElement.getName()+ " \n ");
+            System.out.println(processesElement.getClass1() + " : " + processesElement.getName() + " \n ");
+            Processes += processesElement.getClass1() + ":" + processesElement.getName() + ",";
+            properProcesses = (Processes.substring(0, Processes.length() - 1));
         }
 
-
-        System.out.println("\n" + "Diagram Elements: \n");
+        System.out.println("\n" + "First Diagram Elements: \n");
         for (diagramObject.ContentArray diagramElement : parsedDiagram.getDiagram().getContent().get(0).getContent()) {
             System.out.println(diagramElement.getFrom() + " to " + diagramElement.getTo() + " " + diagramElement.getNode() + "s: [" + diagramElement.getMessage().get(0) + ", " + diagramElement.getMessage().get(1) + ", " + diagramElement.getMessage().get(2) + "] \n ");
+            FirstDiagram += "{" + diagramElement.getFrom() + "," + diagramElement.getTo() + ",[" + diagramElement.getMessage().get(0) + ", " + diagramElement.getMessage().get(1) + ", " + diagramElement.getMessage().get(2) + "]" + "}" + ",";
+            properFirstDiagram = (FirstDiagram.substring(0, FirstDiagram.length()-1));
         }
 
 
@@ -32,7 +51,32 @@ public class Parser {
         System.out.println("\n" + "Parallel Diagram Elements: \n");
         for (diagramObject.ContentArray parallelDiagramElement : parsedDiagram.getDiagram().getContent().get(1).getContent()) {
             System.out.println(parallelDiagramElement.getFrom() + " to " + parallelDiagramElement.getTo() + " " + parallelDiagramElement.getNode() + "s: [" + parallelDiagramElement.getMessage().get(0) + "] \n ");
+            ParallelDiagram += "{" + parallelDiagramElement.getFrom() + "," + parallelDiagramElement.getTo() + ",[" + parallelDiagramElement.getMessage().get(0) +  "]" + "}" + ",";
+            ProperParallelDiagram = (ParallelDiagram.substring(0, ParallelDiagram.length()-1));
         }
-		
+
+        System.out.println("\n" + "Properly Formatted String of First Diagram: \n");
+        System.out.println(getFirstSequenceDiagram());
+        System.out.println("\n" + "Properly Formatted String of Parallel Diagram: \n");
+        System.out.println(getParallelSequenceDiagram());
 	}
+
+
+
+    public static String getFirstSequenceDiagram(){
+
+        String FirstSequenceDiagram = "{" + counter + ",[" + properProcesses + "]," + "["+ properFirstDiagram + "]}";
+        counter++;
+
+        return FirstSequenceDiagram;
+    }
+
+    public static String getParallelSequenceDiagram(){
+
+        String ParallelSequenceDiagram = "{" + parallelCounter + ",[" + properProcesses + "]," + "["+ ProperParallelDiagram + "]}";
+        parallelCounter++;
+
+        return ParallelSequenceDiagram;
+    }
+
 }
