8f1c87c2dbb24d7e6c4c8e54781b77b4122a0f8d
unknown <gustornqis@student.gu.se>
2017-10-26 16:05:45 +0200

    Executionlog backend

--- Server/src/executionlog.erl
+++ Server/src/executionlog.erl
@@ -1,5 +1,32 @@
+-module(executionlog).
 
+%% API
 
-end with tilde ~
+-export([start_server/0]).
+-define(Port, 9000).
 
-{From, To, Msg, Message, Message_number}~
+start_server()->
+	Pid = spawn_link(fun() ->
+		{ok, LSocket} = gen_tcp:listen(?Port, [binary, {active, false}]),
+		spawn(fun()-> acceptState(LSocket) end),
+		timer:sleep(infinity)
+		end),
+	{ok, Pid}.	
+		
+acceptState(LSocket) ->
+	{ok, ASocket} = gen_tcp:accept(LSocket),
+	spawn(fun()-> acceptState(LSocket) end),
+	handler(ASocket).
+
+handler(ASocket)->
+	inet:setopts(ASocket,[{active,once}]),
+	receive
+	{tcp,ASocket,<<"quit">>} ->
+		gen_tcp:close(ASocket);
+	{tcp,ASocket,<<"nodeSend">>} ->
+		gen_tcp:send(ASocket,"Node sent a message"),
+		handler(ASocket);
+	{tcp,ASocket,<<"nodeReceive">>} ->
+		gen_tcp:send(ASocket,"Node received a message"),
+		handler(ASocket)	
+		end.
