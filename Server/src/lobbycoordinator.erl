f767030a21905dd086d8fafedbb219f1334e9085
Unknown <gusfransse@student.gu.se>
2017-11-09 13:34:41 +0100

    Merge conf

--- Server/src/lobbycoordinator.erl
+++ Server/src/lobbycoordinator.erl
@@ -16,15 +16,14 @@ loop(Rooms, Counter) ->
 	  
     {join_lobby, Socket, Lobby_ID} -> ;
 	{leave_lobby, Socket, Lobby_ID} -> not_implemented;
-	{command, Creator_Socket, Lobby_ID, Command} -> not_implemented
+	{command, Creator_Socket, Lobby_ID, Command} -> not_implemented;
 
+	{'DOWN', _Ref, _process, Lid, Reason} -> 
+	  %Send a notice to the Client stating that a lobbt has crashed as well as relay the reason.
+	  Format_result = io_lib:format("~p", [{Lid, lobby_crashed, Reason}])
   end,  
   loop(Rooms).
   
 find_room([], Lobby_ID})                  -> not_created ;
 find_room([{Lobby_ID, Pid}|Ls], Lobby_ID) -> Pid;
 find_room([_|Ls], Lobby_ID)               -> find_room(Ls, Lobby_ID).
-
-	{'DOWN', _Ref, _process, Lid, Reason} -> 
-		%Send a notice to the Client stating that a lobbt has crashed as well as relay the reason.
-		Format_result = io_lib:format("~p", [{Lid, lobby_crashed, Reason}])
