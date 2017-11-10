2b5be63523e1fc868aa48f7892584830f54a33ce
Tim Jonasson <gusjonati@student.gu.se>
2017-11-10 14:58:40 +0100

    first version of the erlang files

--- Server/src/lobby.erl
+++ Server/src/lobby.erl
@@ -1,14 +1,32 @@
 -module(lobby).
 -export([init/1]).
 
-init(Creator_Socket) -> loop(Creator_Socket, [Creator_Socket], []).
+init(Creator_Socket) -> loop(Creator_Socket, [Creator_Socket], [], lobby1).
 
 
-loop(Creator_Socket, Members, Diagrams) -> 
+loop(Creator_Socket, Members, Diagrams, Lobby_ID) -> 
   receive
     {remove_lobby, Creator_Socket} -> ok;
-	{create_diagram, Creator_Socket, {Did, Class_names, Classes, Messages}} -> not_implemented;
-	{command, {Did, next_message}} -> not_implemented;
-	{command, {Did, previous_message}} -> not_implemented;
+	
+	{create_diagram, Creator_Socket, {ok, {Did, Class_names, Classes, Messages}}} -> 
+	  %Sends the class names and messages to the client
+      %The character ~ is used as the stop character for when the client should stop reading from the tcp connection
+      Diagram_ID = Did ++ Lobby_ID,
+	  Format_result = io_lib:format("~p", [{Diagram_ID, Class_names}]) ++ "~",
+      gen_tcp:send(Creator_Socket, Format_result),
+	  Self = self(),
+	  loop(Creator_Socket, Members, [{Diagram_ID, spawn(fun () -> diagramcoordinator:init(Self, Diagram_ID, {Classes, Messages}) end)}| Diagrams], Lobby_ID);
+	
+	{command, Creator_Socket, {ok, {Did, Message_request}}} -> 
+	  Pid = usercoordinator:find_diagram(Did, Diagrams),
+	  %Sends the message to the diagram coordinator.
+	  Pid ! {Message_request, self()},
+	  %Confirmation that the diagram coordinator received the message
+	  receive
+	  	ok -> ok
+	  end,
+	  %Receives the result from the diagram coordinator and sends it to the client
+	  loop(Creator_Socket, Members, Diagrams, Lobby_ID);
+    {remove_diagram, Did} -> not_implemented
   end.
-	{remove_diagram, Did} -> ;
+  
--- Server/src/lobbycoordinator.erl
+++ Server/src/lobbycoordinator.erl
@@ -9,21 +9,23 @@ loop(Rooms, Counter) ->
   receive 
     {create_lobby, Creator_Socket} -> 
 	  %%TODO - Monitor
-	  spawn(fun () -> lobby:init(Creator_Socket) end);
+	  spawn(fun () -> lobby:init(Creator_Socket) end),
+      loop(Rooms, counter);
 	  
 	{remove_lobby, Creator_Socket, Lobby_ID} -> 
-	  Lobby_ID ! {remove_lobby, Creator_Socket};
+	  Lobby_ID ! {remove_lobby, Creator_Socket},
+      loop(Rooms, counter);
 	  
-    {join_lobby, Socket, Lobby_ID} -> ;
+    {join_lobby, Socket, Lobby_ID} -> ok;
 	{leave_lobby, Socket, Lobby_ID} -> not_implemented;
 	{command, Creator_Socket, Lobby_ID, Command} -> not_implemented;
 
 	{'DOWN', _Ref, _process, Lid, Reason} -> 
 	  %Send a notice to the Client stating that a lobbt has crashed as well as relay the reason.
-	  Format_result = io_lib:format("~p", [{Lid, lobby_crashed, Reason}])
-  end,  
-  loop(Rooms).
+	  Format_result = io_lib:format("~p", [{Lid, lobby_crashed, Reason}]), 
+      loop(Rooms, counter)
+  end.
   
-find_room([], Lobby_ID})                  -> not_created ;
+find_room([], Lobby_ID)                  -> not_created ;
 find_room([{Lobby_ID, Pid}|Ls], Lobby_ID) -> Pid;
 find_room([_|Ls], Lobby_ID)               -> find_room(Ls, Lobby_ID).
