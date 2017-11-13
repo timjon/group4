38a5a817c9e26bb8c8dd29d06f0be88c52ae7edc
Unknown <gusfransse@student.gu.se>
2017-11-13 13:32:35 +0100

    Server update for shared session

--- Server/src/lobby.erl
+++ Server/src/lobby.erl
@@ -1,5 +1,7 @@
 -module(lobby).
 -export([init/1]).
+%%Version: 0.1
+%%Collaborators: Sebastian Fransson
 
 init(Creator_Socket) -> loop(Creator_Socket, [Creator_Socket], []).
 
@@ -8,7 +10,8 @@ loop(Creator_Socket, Members, Diagrams) ->
   receive
     {remove_lobby, Creator_Socket} -> ok;
 	{create_diagram, Creator_Socket, {Did, Class_names, Classes, Messages}} -> not_implemented;
+	{remove_diagram, Did} -> not_implemented;
 	{command, {Did, next_message}} -> not_implemented;
-	{command, {Did, previous_message}} -> not_implemented;
+	{command, {Did, previous_message}} -> not_implemented
+	
   end.
-	{remove_diagram, Did} -> ;
