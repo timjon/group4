3d7e355e67e4d9e1ab474af23a30eae9859622ff
Rashadse <rashadse@hotmail.com>
2017-11-14 09:51:37 +0100

    fixed typo

--- Application/src/model/sequenceDiagramParser/Processes.java
+++ Application/src/model/sequenceDiagramParser/Processes.java
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName;
 /**
  * This Class is part of the Sequence Diagram Object for the parser from JSON to POJO (Plain Old Java Objects)
  * The getters and setters and their declarations are made to adhere with a predetermined JSON format
- * This Poetesses class will contain the class names in the parsed sequence diagram
+ * This Processes class will contain the class names in the parsed sequence diagram
  * @author Rashad Kamsheh & Isabelle Törnqvist
  * @version 1.0
  * @since 2017-10-16
