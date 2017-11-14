6bbe74423d7043a95bd39c413bd6bde91172ca64
Rashadse <rashadse@hotmail.com>
2017-11-14 11:18:29 +0100

    removed p tags

--- Application/src/model/Parser.java
+++ Application/src/model/Parser.java
@@ -13,19 +13,19 @@ import java.util.List;
  * @author Rashad Kamsheh & Isabelle Törnqvist
  * @version 1.2
  * @since 2017-10-16
- * <p>
+ *
  * Made with usage of Gson library for parsing json into Java objects
  * https://github.com/google/gson
  * Gson is released under the Apache 2.0 license.
- * <p>
+ *
  * Copyright 2008 Google Inc.
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
