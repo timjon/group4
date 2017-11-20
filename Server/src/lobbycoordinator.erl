576265048850cf0f5ac7eb053616c4c3cff962c8
Sebastian Fransson <gusfransse@student.gu.se>
2017-11-20 14:12:08 +0100

    Small lobby fix

--- Server/src/lobbycoordinator.erl
+++ Server/src/lobbycoordinator.erl
@@ -12,10 +12,9 @@ loop(Rooms, Lobby_increment) ->
   receive 
     {create_lobby, Creator_Socket, Password} -> 
 	  Pid = spawn(fun () -> lobby:init(Creator_Socket, Password, self()) end),
-      monitor(Pid),
-	  Rooms ++ [{Lobby_increment, Password, Pid}],
+      monitor(process, Pid),
 	  created_lobby,
-	  loop(Rooms, Lobby_increment + 1)
+	  loop([{Lobby_increment, Password, Pid}|Rooms], Lobby_increment + 1)
 	  
 	{remove_lobby, Creator_Socket, Lobby_ID} -> 
 	  Lobby_ID ! {remove_lobby, Creator_Socket},
@@ -29,7 +28,7 @@ loop(Rooms, Lobby_increment) ->
 
 	{'DOWN', _Ref, _process, Pid, Reason} -> 
 	  %Send a notice to the Client stating that a lobbt has crashed as well as relay the reason.
-	  Format_result = io_lib:format("~p", [{Lobby_ID, lobby_crashed, Reason}])
+	  Format_result = io_lib:format("~p", [{Pid, lobby_crashed, Reason}])
   end.
   
 find_room([], Lobby_ID)                  -> not_created ;
