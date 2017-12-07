79277945ee5bb61afc2e6f9c83fe31aa8e85bd0f
timjon <gusjonati@student.gu.se>
2017-12-07 12:01:34 +0100

    Update Parser.java

--- Application/src/model/Parser.java
+++ Application/src/model/Parser.java
@@ -12,7 +12,7 @@ import java.util.List;
 /**
  * @author Rashad Kamsheh & Isabelle Törnqvist
  * collaborator: Pontus Laestadius
- * @version 1.4
+ * @version 1.1
  * @since 2017-10-16
  *
  * Made with usage of Gson library for parsing json into Java objects
