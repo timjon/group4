ee0f1d2f1e1ca750d4fff021080de3e59886def5
Tim Jonasson <gusjonati@student.gu.se>
2017-12-13 09:12:22 +0100

    it work

--- Server/src/diagramcoordinator.erl
+++ Server/src/diagramcoordinator.erl
@@ -11,24 +11,31 @@ init(_Coordinator, _Did, {[], _}) -> no_classes;
 init(_Coordinator, _Did, {_, []}) -> no_messages;
 %Spawns and Initializes the diagram coordinator
 init(Coordinator, Did, {L, Messages}) -> 
-    {ok, Socket} = gen_tcp:connect("192.168.0.24", 8041, [binary, {packet, 0}]),
+    Sockets = tcp_connect("192.168.0.24", [8041, 8042, 8043]),
 	%Sending information that the Coordinator has been spawned. To be printed in the executionlog
 	Coordinator ! {Did, print_information, ["Diagram coordinator was spawned"]},
-	Pids = spawn_nodes(Socket, L, Did, Coordinator),
-	loop(Socket, Coordinator, Did, Pids, Messages, 1, []). 
+	Classes = spawn_nodes(Sockets, L, Did, Coordinator, []),
+	loop(Sockets, Coordinator, Did, Classes, Messages, 1, []). 
+	
+tcp_connect(_Ip, []) -> [];
+tcp_connect(Ip,  [Port|Ports]) ->
+  {ok, Socket} = gen_tcp:connect(Ip, Port, [binary, {packet, 0}]),
+  [Socket | tcp_connect(Ip, Ports)].
 
 %This loop runs until the list is empty (when there are no more messages)
-loop(Socket, Coordinator, Did, Pids, NextList, Message_number, PrevList) -> 
+loop(Sockets, Coordinator, Did, Classes, NextList, Message_number, PrevList) -> 
   receive
     {next_message, Coordinator} -> 
 	  case NextList of
 	    [] -> 
 		  Coordinator ! {simulation_done, Did, Message_number},
-	      loop(Socket, Coordinator, Did , Pids, [], Message_number, PrevList);
+	      loop(Sockets, Coordinator, Did , Classes, [], Message_number, PrevList);
 		
 		[L|Ls] -> 
           {From, To, Message} = L,
-	      send_message(Socket, From, To, Message, Message_number, Coordinator, Did), 
+		  FromSocket = find_class(Classes, From),
+		  ToSocket = find_class(Classes, To),
+	      send_message(FromSocket, ToSocket, From, To, Message, Message_number, Coordinator, Did), 
           receive
 		    {tcp, _Socket, Info} ->
 			  case binary_to_term(Info) of
@@ -38,23 +45,26 @@ loop(Socket, Coordinator, Did, Pids, NextList, Message_number, PrevList) ->
 		          Coordinator !  {Did, print_information, ["Node " ++ atom_to_list(To) ++ " received a message from " ++ atom_to_list(From)]}
 			  end
 	      end,
-	      loop(Socket, Coordinator, Did, Pids, Ls, Message_number + 1, [L|PrevList])
+	      loop(Sockets, Coordinator, Did, Classes, Ls, Message_number + 1, [L|PrevList])
 	  end;
 	  
 	{previous_message, Coordinator} ->
 	  case PrevList of
 	    [] -> 
 		  Coordinator ! {Did, print_information, ["No previous message"]},
-	      loop(Socket, Coordinator, Did, Pids, NextList, Message_number, []);
+	      loop(Sockets, Coordinator, Did, Classes, NextList, Message_number, []);
 		  
 	    [Prev_H| Prev_T] -> 
 	      Coordinator ! {previous_confirmation, Did, ["Previous message"]},
-	      loop(Socket, Coordinator, Did, Pids, [Prev_H| NextList], Message_number - 1, Prev_T)
+	      loop(Sockets, Coordinator, Did, Classes, [Prev_H| NextList], Message_number - 1, Prev_T)
 	  end
   end.
 
 %spawns nodes for every class in the list
-spawn_nodes(Socket, List, Did, Coordinator) ->[spawn_node(Socket, Class, Did, Coordinator) || Class <- List].
+spawn_nodes(_, [], _, _, _) -> [];
+spawn_nodes([], Classes, Did, Coordinator, UsedSockets) -> spawn_nodes(UsedSockets, Classes, Did, Coordinator, []);
+spawn_nodes([Socket|Sockets], [Class|Classes], Did, Coordinator, UsedSockets) ->
+  [spawn_node(Socket, Class, Did, Coordinator)| spawn_nodes(Sockets, Classes, Did, Coordinator, [Socket|UsedSockets])].
 
 %spawns a node and returns a tuple with the pid and the class name
 spawn_node(Socket, Class_name, Did, Coordinator) -> 
@@ -62,24 +72,25 @@ spawn_node(Socket, Class_name, Did, Coordinator) ->
   receive
     {tcp, _Socket, Info} -> 
       Coordinator ! binary_to_term(Info)
-  end.
+  end,
+  {Socket, Class_name}.
 
 %sends a message to the given node
-send_message(Socket, From, To, Message, Message_number, Coordinator, Did) ->
-  gen_tcp:send(Socket, term_to_binary({send_message, From, To, Message, Message_number})),
-  message_response(Did, Coordinator).
+send_message(FromSocket, ToSocket, From, To, Message, Message_number, Coordinator, Did) ->
+  gen_tcp:send(FromSocket, term_to_binary({send_message, From, To, Message, Message_number})),
+  message_response(ToSocket, Did, Coordinator).
  
-message_response(Did, Coordinator) ->
+message_response(ToSocket, Did, Coordinator) ->
+  io:format("hello ~n ~p",[ToSocket]),
   receive
-    {tcp, Socket, Info} ->
+    {tcp, _Socket, Info} ->
 	  case binary_to_term(Info) of
         {send_reply, From, To, Message, Message_number} -> 
-		  gen_tcp:send(Socket, term_to_binary({receive_message, From, To, Message, Message_number})),
+		  gen_tcp:send(ToSocket, term_to_binary({receive_message, From, To, Message, Message_number})),
 	      %Sends info to the Coordinator, that the node sucessfully sent a message. To be printed in the executionlog	
 	      Coordinator ! {Did, print_information, ["Node " ++ atom_to_list(From) ++ " sent a message to " ++ atom_to_list(To)]}
       end
   end.
   
-  
-  
-  
+find_class([{Socket, Name}| _Classes], Name) -> Socket;
+find_class([_| Classes], Name) -> find_class(Classes, Name).
--- Server/src/nodebalancer.erl
+++ Server/src/nodebalancer.erl
@@ -1,16 +1,17 @@
 -module(nodebalancer).
--export([main/1, init/0]).
+-export([main/1, init/1]).
 
 %%Author: Tim Jonasson
 %%Version: 1.0
 
 % Makes it possible to initialize the nodes with an escript command
-main(_) -> 
-  init().
+main(String) -> 
+  init(String).
 
 %Initializes the node tcp connection.  
-init() -> 
-  {ok, ListenSocket} = gen_tcp:listen(8041, [{active,true}, binary]),
+init(String) -> 
+  {Port, _} = string:to_integer(hd(String)),
+  {ok, ListenSocket} = gen_tcp:listen(Port, [{active,true}, binary]),
   loop(ListenSocket).
   
 %Accepts connections and spawns a proccess for it
