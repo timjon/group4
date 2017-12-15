a3c6ab865d629b3676682b0154538e4370c9dea7
Tim Jonasson <gusjonati@student.gu.se>
2017-12-15 08:32:57 +0100

    added comments to my files

--- Server/src/diagramcoordinator.erl
+++ Server/src/diagramcoordinator.erl
@@ -3,7 +3,7 @@
 
 %%Author: Tim Jonasson
 %%Collaborators: Isabelle Törnqvist 2017-10-30, Sebastian Fransson 2017-11-06, Pontus Laestadius 2017-12-01
-%%Version: 1.9
+%%Version: 2.0
 
 %Returns no_classes when there was no classes in the given diagram 
 init(_Coordinator, _Did, {[], _}, _) -> no_classes;
@@ -11,13 +11,15 @@ init(_Coordinator, _Did, {[], _}, _) -> no_classes;
 init(_Coordinator, _Did, {_, []}, _) -> no_messages;
 %Spawns and Initializes the diagram coordinator
 
+%Initializes the diagramcoordinator
 init(Coordinator, Did, {Classes, Messages},  Class_names) -> 
-  Sockets = tcp_connect("172.20.10.6", [8041, 8042, 8043]),
+  Sockets = tcp_connect("10.0.151.42", [8041, 8042, 8043]),
   %Sending information that the Coordinator has been spawned. To be printed in the executionlog
   Coordinator ! {Did, print_information, ["Diagram coordinator was spawned"]},
   Nodes = spawn_nodes(Sockets, Classes, Did, Coordinator, []),
   loop(Sockets, Coordinator, Did, Nodes, Messages, 1, [], none, Class_names, none). 
 
+%Creates a tcp connection to every port in the list
 tcp_connect(_Ip, []) -> [];
 tcp_connect(Ip,  [Port|Ports]) ->
   {ok, Socket} = gen_tcp:connect(Ip, Port, [binary, {packet, 0}]),
@@ -46,18 +48,25 @@ loop(Sockets, Coordinator, Did, Classes, NextList, Message_number, PrevList, Cla
   	  Pid ! {class_diagram_request, ClassDiagram},
 	  loop(Sockets, Coordinator, Did, Sockets, Classes, Message_number, PrevList, ClassDiagram, Class_names, DeploymentDiagram);
   	
+	%Simulates the next message in the diagram
     {next_message, Coordinator} -> 
+	  %Checks if there is a message to simulate
 	  case NextList of
 	    [] -> 
 		  Coordinator ! {simulation_done, Did, Message_number},
 	      loop(Sockets, Coordinator, Did , Classes, [], Message_number, PrevList, ClassDiagram, Class_names, DeploymentDiagram);
 		
 		[L|Ls] -> 
+		  %Gets the necessary information from the Message
           {From, To, Message} = L,
+		  
+		  %Finds the sockets
 		  FromSocket = find_class(Classes, From),
 		  ToSocket = find_class(Classes, To),
 	      send_message(Classes, FromSocket, ToSocket, From, To, Message, Message_number, Coordinator, Did, get_class_diagram_id(ClassDiagram)), 
-          receive
+          
+		  %Receives the answer
+		  receive
 		    {tcp, _Socket, Info} ->
 			  case binary_to_term(Info) of
 	            {message_done, From, To, Message, Message_number} ->
@@ -69,7 +78,9 @@ loop(Sockets, Coordinator, Did, Classes, NextList, Message_number, PrevList, Cla
 	      loop(Sockets, Coordinator, Did, Classes, Ls, Message_number + 1, [L|PrevList], ClassDiagram, Class_names, DeploymentDiagram)
 	  end;
 	  
+	%Makes the simulation take a step back
 	{previous_message, Coordinator} ->
+	  %Checks if there is a previous message
 	  case PrevList of
 	    [] -> 
 		  Coordinator ! {Did, print_information, ["No previous message"]},
@@ -108,7 +119,8 @@ send_message(Classes, FromSocket, ToSocket, From, To, Message, Message_number, C
   gen_tcp:send(FromSocket, term_to_binary({send_message, From, To, Message, Message_number})),
   message_response(ToSocket, Did, Coordinator).
  
-message_response(ToSocket, Did, Coordinator) ->
+%Is used for forwarding a message from a node to another node
+ message_response(ToSocket, Did, Coordinator) ->
   receive
     {tcp, _Socket, Info} ->
 	  case binary_to_term(Info) of
@@ -121,9 +133,9 @@ message_response(ToSocket, Did, Coordinator) ->
 
 % Iterates over the Pids and classes and propogates the data to the Pid.
 name_classes(Names, Classes) -> 
-	% Returns if all of the names were matched or any errors.
-	Result = [iter_split(Names, Classes)],
-	atom_to_list(if_err(Result)).
+  % Returns if all of the names were matched or any errors.
+  Result = [iter_split(Names, Classes)],
+  atom_to_list(if_err(Result)).
 	
 % Return if the list has the atom 'err' in it or not.
 if_err([]) -> ok;
@@ -133,8 +145,8 @@ if_err([_ | Rest]) -> if_err(Rest).
 
 % Iterates over the names and splits them to from Class:Name to {Class, [Name]}.  Then matches the Pid with the names.
 iter_split(Names, Classes) -> 
-	% Call the matching function.
-	iter_match( 
+  % Call the matching function.
+  iter_match( 
 		[{
 			% Gets the class.
 			hd(string:split(Name, atom_to_list(':'))), 
@@ -145,7 +157,6 @@ iter_split(Names, Classes) ->
 
 % Matches a node's name with a class.
 iter_match([], _) -> ok;
-
 iter_match([{Class, [Name]} | Rest], Classes) -> 
   Socket = find_class(Classes, list_to_atom(Name)),
   gen_tcp:send(Socket, term_to_binary({setClass, Class, Name})),
@@ -157,25 +168,27 @@ notify_class_diagram(Classes, Coordinator, Did, ClassDiagramId, Name) ->
 	  % If there is a class diagram 
   case ClassDiagramId of 
   % There is no class diagram.
-   none -> none;
-   % Catches all.	
-   _ -> 
-		% Gets the name of the class with it's Pid.
-		Coordinator ! {class_diagram, ClassDiagramId, Did, highlight, getClass(find_class(Classes, Name), Name)}
-	end.
+  none -> none;
+  % Catches all.	
+  _ -> 
+   	% Gets the name of the class with it's Pid.
+	Coordinator ! {class_diagram, ClassDiagramId, Did, highlight, getClass(find_class(Classes, Name), Name)}
+  end.
   
+%Given a list of the classes and a name it returns the socket of the given name
 find_class([{Socket, Name}| _Classes], Name) -> Socket;
 find_class([_| Classes], Name) -> find_class(Classes, Name).
 
 % Gets the class of a specific node.
 getClass(Socket, Name) ->
-	gen_tcp:send(Socket, term_to_binary({getClass, Name})),
-	receive 
-	  {tcp, Socket, Info} ->
-	    case binary_to_term(Info) of
-		  {getClass, Class} -> Class
-		end
-	after
-		1000 ->
-		  err
-	end.
+  gen_tcp:send(Socket, term_to_binary({getClass, Name})),
+  %receives the result
+  receive 
+    {tcp, Socket, Info} ->
+      case binary_to_term(Info) of
+   	    {getClass, Class} -> Class
+	  end
+  after
+	1000 ->
+	  err
+  end.
